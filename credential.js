c92088ef61178f21ccab0a142bc8e2605609a56b
Divyaansh Papreja <knight@Divyaanshs-MacBook-Pro.local>
2020-11-04 15:29:10 +0530

    updated

---
+++ credential.js
@@ -0,0 +1,5 @@
+module.exports.email =  "paprejadivyansh@gmail.com" ;
+
+module.exports.password = "pelvzurijypzbsja";
+
+module.exports.db = "mongodb+srv://admin:123abc@cluster0-fm3f2.mongodb.net/test?retryWrites=true&w=majority";
